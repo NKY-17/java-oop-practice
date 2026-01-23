cabbcae963d3339565f03d39021ee34f4256c42b
NKY-17 <nujahirkamal17@gmail.com>
2026-01-24 00:38:12 +0600

    Task03.java

--- Input, Output, Operators, and Branching/Task03.java
+++ Input, Output, Operators, and Branching/Task03.java
@@ -1,3 +1,4 @@
+/*
 Task 3
 Write a Java program that displays the 2 rightmost digits of your student ID in reverse order. For example, if your student id is 23221454, you need to print 4, and then 5.
 
@@ -5,7 +6,7 @@ Sample Input: 23221454
 Output:
 4
 5
-
+*/
 Code:
 public class task3{
   public static void main(String[]args){
