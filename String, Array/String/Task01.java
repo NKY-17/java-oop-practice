7b7d99c477f00e88b2cce0d9fedf9e02aa0f0092
NKY-17 <nujahirkamal17@gmail.com>
2026-01-25 16:14:03 +0600

    Task01.java

---
+++ String, Array/String/Task01.java
@@ -0,0 +1,34 @@
+/*Task - 01: Write a Java program that will take one string input from the user. Then check and print whether it is a palindrome. 
+
+Sample Input
+Output
+Java
+Not a palindrome
+madam
+Palindrome
+
+*/
+//Code:
+import java.util.Scanner;
+public class task1{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in); 
+String s="";
+String s1="";
+String x=sc.nextLine();
+for(int i=0;i<x.length();i++){
+  char c=x.charAt(i);
+  s+=c;
+}
+for(int j=x.length()-1;j>=0;j--){
+char c1=x.charAt(j);
+s1+=c1;
+}
+if(s.equals(s1)){
+  System.out.println("Palindrome");
+}
+else{
+  System.out.println("Not a Palindrome");
+}
+}
+}
